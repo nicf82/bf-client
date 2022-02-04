176ebd4ad2804dff6665df52b7ac6f2f35167b20
Nic Fellows <1664536+nicf82@users.noreply.github.com>
2022-02-04 10:42:35 +0000

    Tolerant parsing of spf field

---
+++ src/test/scala/net/carboninter/models/ModelSpec.scala
@@ -0,0 +1,42 @@
+package net.carboninter.models
+
+import net.carboninter.appconf.AppConfigService
+import net.carboninter.logging.LoggerAdapter
+import net.carboninter.models.Credentials.Payload
+import net.carboninter.models.{Credentials, SocketDescriptor}
+import net.carboninter.services.{BetfairIdentityService, BetfairStreamService}
+import swagger.definitions.*
+import swagger.definitions.StatusMessage.StatusCode.*
+import zio.test.{TestEnvironment, *}
+import zio.stream.*
+import zio.Duration.*
+import zio.*
+
+import java.time.Instant
+import zio.Duration.*
+import io.circe.*
+import io.circe.parser.*
+import io.circe.syntax.*
+
+import java.io.File
+import java.net.URL
+import scala.io.Source
+
+object ModelSpec  extends DefaultRunnableSpec:
+
+
+
+  def parseTest(file: File) = test("Successfully decode test response in: " + file.getName) {
+
+    val raw = Source.fromFile(file).mkString
+    val result = decode[ResponseMessage](raw)
+
+    assert(result)(Assertion.isRight)
+  }
+
+  lazy val dir = new File(getClass.getResource(s"/testResponses").getFile)
+  lazy val files = dir.listFiles()
+
+  override lazy val spec = suite(getClass.getCanonicalName)(files.map(parseTest): _*).provide(
+    liveEnvironment
+  )
